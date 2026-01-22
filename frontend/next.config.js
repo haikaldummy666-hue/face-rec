928c8459fce71e5ef58be55a46bab03b8e331e12
personal-22 <personal-22@github.com>
2026-01-22 17:33:22 +0700

    Improve: Add swcMinify optimization to next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config) => {
+  swcMinify: true,
+  webpack: (config, { isServer }) => {
     config.resolve.fallback = {
       ...config.resolve.fallback,
       fs: false,
